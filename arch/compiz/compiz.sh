062a945fc46db03cab32fd95e35a499232489955
Benjamin Martinez <cruznick@archlinux.us>
2011-05-02 07:53:04 -0700

    update

---
+++ arch/compiz/compiz.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Set a background color
+BG="#ffffff"
+if which hsetroot >/dev/null 2>&1; then
+    BG=hsetroot
+else
+    if which esetroot >/dev/null 2>&1; then
+	BG=esetroot
+    else
+	if which xsetroot >/dev/null 2>&1; then
+	    BG=xsetroot
+	fi
+    fi
+fi
+test -z $BG || $BG -solid "#202020"
+
+# D-bus
+if which dbus-launch >/dev/null 2>&1 && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+    eval `dbus-launch --sh-syntax --exit-with-session`
+fi
+
+# Run XDG autostart things.  By default don't run anything desktop-specific
+# See xdg-autostart --help more info
+DESKTOP_ENV="COMPIZ"
+if which /usr/bin/xdg-autostart >/dev/null 2>&1; then
+  /usr/bin/xdg-autostart $DESKTOP_ENV
+fi
+
+export vblank_mode=0 
+
+thunar --daemon &
+python2 ~/bin/mpd_infos.py &
+fusion-icon &
+volwheel &
+sleep 4
+parcellite &
+sleep 1
+#Disable Nautilus desktop.
+gconftool-2 -s -t bool /apps/nautilus/preferences/show_desktop false &
+# Do not let Nautilus set the background image.
+gconftool-2 -s -t bool /desktop/gnome/background/draw_background false &
+# Make Nautilus show the advanced permissions dialog
+gconftool-2 -s -t bool /apps/nautilus/preferences/show_advanced_permissions true &
+covergloobus &
+tilda &
+avant-window-navigator
--- arch/openbox/autostart.sh
+++ arch/openbox/autostart.sh
@@ -21,15 +21,6 @@ if which dbus-launch >/dev/null 2>&1 && test -z "$DBUS_SESSION_BUS_ADDRESS"; the
        eval `dbus-launch --sh-syntax --exit-with-session`
 fi
 
-# Make GTK apps look and behave how they were set up in the gnome config tools
-#if test -x /usr/libexec/gnome-settings-daemon >/dev/null; then
-#  /usr/libexec/gnome-settings-daemon &
-#elif which gnome-settings-daemon >/dev/null 2>&1; then
-#  gnome-settings-daemon &
-# Make GTK apps look and behave how they were set up in the XFCE config tools
-#elif which xfce-mcs-manager >/dev/null 2>&1; then
-#  xfce-mcs-manager n &
-#fi
 
 # Preload stuff for KDE apps
 if which start_kdeinit >/dev/null 2>&1; then
@@ -42,4 +33,23 @@ DESKTOP_ENV="OPENBOX"
 if which /usr/lib/openbox/xdg-autostart >/dev/null 2>&1; then
   /usr/lib/openbox/xdg-autostart $DESKTOP_ENV
 fi
-ob-autostart -d &
+
+###Start apps
+xcom set
+cp ~/.config/openbox/gtkrc-2.0  ~/.gtkrc-2.0 &
+tint2 &
+
+sleep 
+nitrogen --restore &
+thunar --daemon &
+
+sleep 3
+volwheel &
+
+sleep 1
+parcellite &
+python2 ~/bin/mpd_infos.py &
+tilda &
+sleep 2
+
+
